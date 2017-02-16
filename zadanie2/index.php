3a574be29656cfc7719d2c4c6c470e4c6eb51b86
vagabondi <kamil.mateusiak.gmail.com>
2017-02-16 21:22:24 +0100

    second task - completed

--- zadanie2/index.php
+++ zadanie2/index.php
@@ -6,13 +6,14 @@ function getValues($path)
         $i = -1;
         $j = 0;
         $terrains = [];
+        $sizes = [];
         while (($line = fgets($input)) !== false) {
             $clearedLine =  preg_replace('/\s+/', '',$line);
             if(strlen($clearedLine)===1) {
                 $i++;
                 $j = 0;
-                $size = intval(strlen($clearedLine));
-                $first_one = $terrains[$i];
+                $size = intval($clearedLine);
+                $sizes[] = $size;
             } else {
                 $myArray = explode(',', $clearedLine);
                 $myArr = array_map('intval', $myArray);
@@ -22,13 +23,48 @@ function getValues($path)
         }
     fclose($input);
 
-    return $terrains;
+    return [
+      'sizes' => $sizes,
+      'terrains' => $terrains
+    ];
 
     } else {
-
         return 'Error opening the file.';
-
     }
 }
 
-getValues('./file.txt');
+function getConsumedEnergy($sizes, $terrains)
+{
+    $energies = [];
+    foreach($sizes as $index => $size) {
+        $i = 0;
+        $j = 0;
+        $terrain = $terrains[$index];
+        $energy = $terrain[$i][$j];
+
+        while($i < $size || $j < $size) {
+            $horrizontal = $terrain[$i][$j+1];
+            $vertical = $terrain[$i+1][$j];
+
+            if((!is_null($horrizontal) && $horrizontal <= $vertical && $j+1 < $size)) {
+                $energy += $horrizontal;
+                $j++;
+            } elseif(!is_null($vertical) && $i+1 < $size) {
+                $energy += $vertical;
+                $i++;
+            } elseif((is_null($vertical) && !is_null($horrizontal) && $j+1 < $size)) {
+                $energy += $horrizontal;
+                $j++;
+            }  else {
+                $i=$size;
+                $j=$size;
+            }
+        }
+        $energies[] = $energy;
+    }
+    return $energies;
+}
+
+$array = getValues('./file.txt');
+
+getConsumedEnergy($array['sizes'], $array['terrains']);
